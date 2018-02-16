83aa93d184ed6a8b605e4ca116d8ae9aa2cd7a37
Yakima <cleveryun@163.com>
2018-02-16 22:31:59 +0800

    修改首页文案

--- src/htmls/pages/root/index/page.js
+++ src/htmls/pages/root/index/page.js
@@ -3,7 +3,7 @@ $utils.hook.ready(() => {
 
   $startPlay.click(function (e) {
     $utils.modal.alert({
-      content: '欢迎访问Djax项目，这是一个很纯粹的页面，请替换成你自己的页面，开始你的表演。'
+      content: '欢迎访问Djax项目，这是一个很纯粹模板，请替换成你自己的内容，开始你的表演。'
     })
   })
 })
