e4583fc074691627092581ae2a1bb00dfa84154f
Yakima <cleveryun@163.com>
2018-02-16 22:24:36 +0800

    恢复误删除的首页点击按钮后的弹框

--- src/htmls/pages/root/index/page.js
+++ src/htmls/pages/root/index/page.js
@@ -1,3 +1,9 @@
 $utils.hook.ready(() => {
-  //
+  const $startPlay = $('#startPlay')
+
+  $startPlay.click(function (e) {
+    $utils.modal.alert({
+      content: '欢迎访问Djax项目，这是一个很纯粹的页面，请替换成你自己的页面，开始你的表演。'
+    })
+  })
 })
