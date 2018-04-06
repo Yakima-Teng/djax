6aacf2085613a67352f49124a72ad55aabd4e29c
Yakima <cleveryun@163.com>
2018-04-06 13:03:05 +0800

    update tip text

--- src/htmls/pages/root/index/page.js
+++ src/htmls/pages/root/index/page.js
@@ -2,8 +2,9 @@ $utils.hook.ready(() => {
   const $startPlay = $('#startPlay')
 
   $startPlay.click(function (e) {
-    $utils.modal.alert({
-      content: '欢迎访问Djax项目，这是一个很纯粹的模板，请替换成你自己的内容，开始你的表演。'
+    $utils.modal.confirm({
+      content: '欢迎访问Djax项目，这是一个很纯粹的模板，请替换成你自己的内容。<br />温馨提示：点击确定按钮可到源码页面查看详细文档！',
+      yes () { window.location.href = 'https://github.com/Yakima-Teng/djax' }
     })
   })
 })
